a0803aba6fb90ed33d755d28bfa594440cd4519f
andre wijaya <andrewijaya988@gmail.com>
2025-08-30 10:47:17 +0700

    style: make address section responsive

--- components/landing/Address.tsx
+++ components/landing/Address.tsx
@@ -3,7 +3,7 @@ import { Church } from "lucide-react";
 export default function Address() {
   return (
     <section className="py-24">
-      <div className="container flex justify-between items-center mx-auto">
+      <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto">
         <div className="text-center">
           <Church className="text-accent size-24 block mx-auto" />
           <h1 className="text-accent font-extrabold text-4xl">GMS Lampung</h1>
