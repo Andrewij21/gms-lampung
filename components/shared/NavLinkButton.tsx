6d966c4dcaa25d76d36ef4b0415edcc6e8b36e06
andre wijaya <andrewijaya988@gmail.com>
2025-08-30 10:46:57 +0700

    style: add undeline animation

--- components/shared/NavLinkButton.tsx
+++ components/shared/NavLinkButton.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { motion } from "framer-motion";
 
 export default function NavLinks({
   title,
@@ -11,9 +13,27 @@ export default function NavLinks({
   className?: string;
 }) {
   return (
-    <Button asChild variant="link" size={"sm"}>
+    <Button
+      asChild
+      variant="ghost"
+      size={"sm"}
+      className="hover:bg-transparent hover:text-white "
+    >
       <Link href={`/${url}`} className={`${className} text-white text-lg`}>
-        {title}
+        <motion.span
+          initial={{ backgroundSize: "0% 2px" }}
+          whileHover={{ backgroundSize: "100% 2px" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          style={{
+            backgroundImage: "linear-gradient(currentColor, currentColor)",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "0 100%",
+            backgroundSize: "0% 2px",
+            display: "inline-block",
+          }}
+        >
+          {title}
+        </motion.span>
       </Link>
     </Button>
   );
