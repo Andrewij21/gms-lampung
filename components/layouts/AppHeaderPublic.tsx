5cdfb0d7731a6e707791145f8c918229b9aba9e2
andre wijaya <andrewijaya988@gmail.com>
2025-09-04 17:44:47 +0700

    feat: add background to header if scroll y>50

--- components/layouts/AppHeaderPublic.tsx
+++ components/layouts/AppHeaderPublic.tsx
@@ -1,10 +1,33 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavLinks from "../shared/NavLinkButton";
 
 function AppHeaderPublic() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // You can adjust the scroll threshold (e.g., 50)
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="flex justify-between items-center py-6 px-32 top-0 mx-auto fixed w-full z-50">
+    <header
+      className={`fixed top-0 z-50 w-full py-6 px-32 flex justify-between items-center transition-all duration-500
+        ${isScrolled ? "bg-primary" : "bg-transparent"}`}
+    >
       <Image src="/logo.png" alt="Logo" width={100} height={100} className="" />
       <div className="text-white">
         <ul className="flex ">
